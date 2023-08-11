9134257a4fcc9b24c5b077f6bced9a43460de4bf
Egor Gulyaev <egor.gulaev22@gmail.com>
2023-08-11 19:39:15 +0700

    modal window commit

--- src/js/_dialog.js
+++ src/js/_dialog.js
@@ -6,3 +6,38 @@ if (!document.createElement('dialog').showModal) {
         .forEach(dialogPolyfill.registerDialog)
     )
 }
+
+const modalTrigger = document.querySelector('.entry-btn')
+const dialog = document.querySelector('.entry-window')
+const closeButton = document.querySelector('.entry-window__btn-close')
+
+modalTrigger.addEventListener("click", openModalAndLockScroll);
+dialog.addEventListener("close", returnScroll);
+closeButton.addEventListener('click', closeModal)
+
+function openModalAndLockScroll() {
+  dialog.showModal();
+  document.body.classList.add("scroll-lock");
+}
+
+function returnScroll() {
+  document.body.classList.remove("scroll-lock");
+}
+
+function closeModal() {
+  dialog.close();
+  document.body.classList.remove("scroll-lock");
+}
+
+dialog.addEventListener("click", e => {
+  const dialogDimensions = dialog.getBoundingClientRect()
+  if (
+    e.clientX < dialogDimensions.left ||
+    e.clientX > dialogDimensions.right ||
+    e.clientY < dialogDimensions.top ||
+    e.clientY > dialogDimensions.bottom
+  ) {
+    dialog.close()
+  }
+})
+
